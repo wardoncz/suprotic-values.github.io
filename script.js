a2831430d0b280ba6f1266fba8fea43dcafd73ea
wardoncz <89449725+wardoncz@users.noreply.github.com>
2023-12-09 14:36:24 +0100

    Update

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 var navState = false
-var valuesBox = document.getElementById('values-box')
-valuesBox.style.opacity = '0'
-valuesBox.style.marginTop = '-150%'
+
 
 function navClick(){
     var navIcon = document.getElementById('nav-icon');
@@ -9,35 +7,13 @@ function navClick(){
     var navIconSec = document.getElementById('navSec')
     var navIconLas = document.getElementById('navLas')
     var nav = document.getElementById('nav')
-    var homeBg = document.getElementById('home')
 
     if (navState == false){
         nav.style.marginLeft = "0"
-        homeBg.style.filter = ''
         navState = true
     } else  {
         nav.style.marginLeft = "-50%"
         navState = false
     }
     
-}
-
-var valuesState = false
-
-function valuesClick(){
-    var vluesBtn = document.getElementById('values-btn-arrow')
-
-    if (valuesState == false){
-        valuesBox.style.opacity = '100%'
-        valuesBox.style.marginTop = '0'
-        vluesBtn.style.transform = 'rotate(180deg)'
-        vluesBtn.style.padding = '0 20px 5px 0' 
-        valuesState = true
-    } else  {
-        valuesBox.style.opacity = '0'
-        valuesBox.style.marginTop = '-150%'
-        vluesBtn.style.transform = 'rotate(0)'
-        vluesBtn.style.padding = '5px 0 0 20px' 
-        valuesState = false
-    }
 }
