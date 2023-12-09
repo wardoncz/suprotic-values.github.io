c8b8c1f5d779b292664e47049187058d76bf26ab
wardoncz <89449725+wardoncz@users.noreply.github.com>
2023-12-09 16:55:21 +0100

    Update

--- script.js
+++ script.js
@@ -16,17 +16,17 @@ function navClick(){
         nav.style.marginLeft = "-50%"
         navState = false
     }
-    
 }
+
 
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-const STAR_COUNT = 200
+const STAR_COUNT = 300
 let result = ""
 for(let i = 0; i < STAR_COUNT; i++){
-  result += `${randomNumber(-50, 50)}vw ${randomNumber(-50, 50)}vh ${randomNumber(0, 1)}px ${randomNumber(0, 1)}px #adb7c7,`
+  result += `${randomNumber(-50, 50)}vw ${randomNumber(-50, 50)}vh ${randomNumber(0, 1)}px ${randomNumber(0, 1)}px #fff,`
 }
 console.log(result.substring(0, result.length - 1))
 stars.style.boxShadow = result.substring(0, result.length - 1)
