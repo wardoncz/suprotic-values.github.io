aff63da8bca3822e3950cf66d3ef4b3565deab68
wardoncz <89449725+wardoncz@users.noreply.github.com>
2023-12-10 11:31:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -40,5 +40,5 @@ let result = ""
 for(let i = 0; i < STAR_COUNT; i++){
   result += `${randomNumber(-50, 50)}vw ${randomNumber(-50, 50)}vh ${randomNumber(0, 1)}px ${randomNumber(0, 1)}px #fff,`
 }
-console.log(result.substring(0, result.length - 1))
+
 stars.style.boxShadow = result.substring(0, result.length - 1)
