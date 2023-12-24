3bd6f66f6f81cec747422816fd19bb996656fc08
wardoncz <89449725+wardoncz@users.noreply.github.com>
2023-12-24 13:00:57 +0100

    Better particles

---
+++ stars.js
@@ -0,0 +1,92 @@
+particlesJS('bg', {
+    particles: {
+        number: {
+            value: 80,
+            density: {
+                enable: true,
+                value_area: 800
+            }
+        },
+        color: {
+            value: '#ffffff'
+        },
+        shape: {
+            type: 'circle', // Set the shape to 'circle'
+            stroke: {
+                width: 0,
+                color: '#000000'
+            },
+            polygon: {
+                nb_sides: 5
+            }
+        },
+        opacity: {
+            value: 0.8,
+            random: true,
+            anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: false
+            }
+        },
+        size: {
+            value: 3,
+            random: true,
+            anim: {
+                enable: true,
+                speed: 2,
+                size_min: 0.1,
+                sync: false
+            }
+        },
+        line_linked: {
+            enable: false
+        },
+        move: {
+            enable: true,
+            speed: 0.3,
+            direction: 'random',
+            random: true,
+            straight: false,
+            out_mode: 'out',
+            bounce: false
+        }
+    },
+    interactivity: {
+        detect_on: 'canvas',
+        events: {
+            onhover: {
+                enable: false
+            },
+            onclick: {
+                enable: false
+            },
+            resize: true
+        },
+        modes: {
+            grab: {
+                distance: 200,
+                line_linked: {
+                    opacity: 1
+                }
+            },
+            bubble: {
+                distance: 200,
+                size: 80,
+                duration: 0.4
+            },
+            repulse: {
+                distance: 200,
+                duration: 0.4
+            },
+            push: {
+                particles_nb: 4
+            },
+            remove: {
+                particles_nb: 2
+            }
+        }
+    },
+    retina_detect: true
+});
